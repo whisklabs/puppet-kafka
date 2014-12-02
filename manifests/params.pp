87f3bb521d819fe65ce3a04881c1de0974b1f9d5
Pedro Pessoa <prp@pobox.com>
2014-12-02 15:47:20 +0000

    add statsd support

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,5 +5,7 @@ class kafka::params {
   $package_url = hiera('kafka:package_url', undef)
   $install_dir = hiera('kafka:install_dir', '/usr/local/kafka')
   $hostname    = hiera('kafka:hostname', $::ipaddress)
+  $statsd_host = hiera('kafka:statsd_host', $::statsd_host)
+  $statsd_port = hiera('kafka:statsd_port', $::statsd_port)
   $zookeeper_connect = hiera('kafka:zookeeper_connect', 'localhost:2181')
 }
